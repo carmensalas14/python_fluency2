3cb592e8082a50ece8338aa7605a251a3a39e23f
EC2 Default User <ec2-user@ip-172-31-24-236.us-east-2.compute.internal>
2020-01-22 14:44:31 +0000

    complete challenge

---
+++ python_fluency2.py
@@ -0,0 +1,48 @@
+import re
+
+def halved(lst):
+    new_lst = []
+    for num in lst:
+        new_lst.append(num/2)
+    return new_lst
+    
+def only_positive(lst):
+    new_lst =[]
+    for num in lst
+        if num > 0:
+            new_lst.append(num)
+    return new_lst
+    
+def sum(lst):
+    sum = 0
+    for num in lst:
+        sum += num
+    return sum
+    
+def stripped_strings(lst):
+    new_lst = []
+    for string in lst:
+        if string = re.sub('\W', '', lst)
+        string.append(new_lst)
+    return new_lst
+    
+def find_special(lst):
+    for i in range(len(lst)):
+        if string == 'special'
+            return i
+        elif:
+            return -1
+            
+def valid_contacts(contacts):
+    new_lst =[]
+    for contact in contatcs:
+        if len(contact.phone_number) == 10:
+            return append.(contact)
+            
+def contact_names(contacts):
+    new_lst=[for contact in contacts return contact.name]
+        
+    
+            
+    
+    
